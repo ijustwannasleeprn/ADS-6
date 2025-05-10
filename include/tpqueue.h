6e6e82f6794d8b9bd379c0316a4dda5f3b422cca
Oleg <byosipow.second@gmail.com>
2025-05-10 14:59:02 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,82 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+#include <iostream>
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
+};
+
+template <typename T>
+class TPQueue {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& data, Node* next = nullptr) : data(data), next(next) {}
+    };
+    Node* head;
+    Node* tail;
+    size_t size;
+public:
+    TPQueue() : head(nullptr), tail(nullptr), size(0) {}
+    ~TPQueue() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+    void push(const T& item) {
+        Node* newNode = new Node(item);
+        if (!head || item.prior > head->data.prior) {
+            newNode->next = head;
+            head = newNode;
+            if (!tail) tail = head;
+            size++;
+            return;
+        }
+        Node* current = head;
+        while (current->next && current->next->data.prior >= item.prior) {
+            current = current->next;
+        }
+        newNode->next = current->next;
+        current->next = newNode;
+        if (!newNode->next) {
+            tail = newNode;
+        }
+        size++;
+    }
+    T pop() {
+        if (!head) {
+            throw std::runtime_error("Queue is empty");
+        }
+        Node* temp = head;
+        T data = temp->data;
+        head = head->next;
+        if (!head) {
+            tail = nullptr;
+        }
+        delete temp;
+        size--;
+        return data;
+    }
+
+    void print() const {
+        Node* current = head;
+        while (current) {
+            std::cout << current->data.ch << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+    size_t getSize() const {
+        return size;
+    }
+    bool isEmpty() const {
+        return size == 0;
+    }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
